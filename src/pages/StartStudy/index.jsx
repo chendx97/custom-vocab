53923a184a9582fa976993ea0703581c47fdd82f
chendx97 <3310115926@qq.com>
2024-03-24 16:58:32 +0800

    feat: StartStudy布局

--- src/pages/StartStudy/index.jsx
+++ src/pages/StartStudy/index.jsx
@@ -1,6 +1,29 @@
+import { Button } from 'antd';
+import styles from './index.module.less';
+
 function StartStudy() {
   return (
-    <span>hi</span>
+    <div className={styles.startStudy}>
+      <div className="content-wrapper">
+        <div className="card">
+          <div className="word">hello</div>
+          <div className="explain-list">
+            <div className="explain-item">aaaaaaaa</div>
+            <div className="explain-item">aaaaaaaa</div>
+          </div>
+        </div>
+        <div className="switch-wrapper">
+          <Button className='btn-switch'>上一个</Button>
+          <Button className='btn-switch'>下一个</Button>
+        </div>
+        <div className="opera-wrapper">
+          <Button className='btn-opera btn-tip'>提示</Button>
+          <Button className='btn-opera btn-simple'>标记为简单</Button>
+          <Button className='btn-opera btn-difficult'>标记为困难</Button>
+          <Button className='btn-opera btn-delete'>删除</Button>
+        </div>
+      </div>
+    </div>
   )
 }
 
