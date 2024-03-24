cc9f83bf9a80def168ca31bd9ac921ff774d1193
chendx97 <3310115926@qq.com>
2024-03-24 12:42:29 +0800

    feat: 添加less

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +9,12 @@ export default defineConfig({
     host: '127.0.0.1',
     port: 3333,
   },
+  css: {
+    preprocessOptions: {
+      less: {
+        javascriptEnabled: true,
+        additionalData: `@import "${path.resolve(__dirname, "./src/assets/global-var.less")}";`,
+      }
+    }
+  }
 })
