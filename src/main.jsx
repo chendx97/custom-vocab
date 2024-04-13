537608be935d20d687c560b7a73d1a9588036ec7
chendx97 <3310115926@qq.com>
2024-04-13 11:27:14 +0800

    style: 引入tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './router.jsx';
 import './utils/myFetch.js';
-import './index.less';
+import './index.css';
 import './assets/custom-ant.less';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
